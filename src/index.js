df9b912bce92adb14834c591757b6227b9679716
tttman <tttman92@gmail.com>
2020-10-17 18:58:45 +0800

    EditorContainerLayout

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@
 //   const [ok, setOk] = useState(0)
 //   return <div className={styles.test}>Example Component: {text} {ok}</div>
 // }
+import EditorContainerLayout from './editor-container-layout'
 
 export { default as SideBarContainer } from './sidebar-container'
 export { default as SidenavLayout } from './sidenav-layout'
 export { default as ContainerLayout } from './container-layout'
+
+export { EditorContainerLayout }
+
+const commonsUI = {
+  EditorContainerLayout
+}
+export default { EditorContainerLayout, commonsUI }
