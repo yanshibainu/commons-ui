c9ca3ad3e477a34e5d73524eda5fc427441a0871
tttman <tttman92@gmail.com>
2020-11-09 19:47:22 +0800

    加react-split-pane

--- src/editor-container-layout/editor-container-layout.js
+++ src/editor-container-layout/editor-container-layout.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Row, Col } from 'rsuite'
-
+import { Grid, Row } from 'rsuite'
+import SplitPane from 'react-split-pane'
 const propTypes = {
-  menuBar: PropTypes.elementType.isRequired,
-  contextLayout: PropTypes.elementType.isRequired,
-  toolBar: PropTypes.elementType.isRequired,
-  leftLayout: PropTypes.elementType.isRequired
+  menuBar: PropTypes.object.isRequired,
+  contextLayout: PropTypes.object.isRequired,
+  toolBar: PropTypes.object.isRequired,
+  leftLayout: PropTypes.object.isRequired
 }
 
 const defaultProps = {}
-
+const styles = {
+  background: '#000',
+  width: '2px',
+  cursor: 'col-resize',
+  margin: '0 5px',
+  height: '100%'
+}
 const EditContainerLayout = (props) => {
   return (
     <div>
       <Grid fluid>
         <Row>{props.menuBar}</Row>
-        <Row>{props.toolBar}</Row>
         <Row>
-          <Col xs={6}>{props.leftLayout}</Col>
-          <Col xs={18}>{props.contextLayout}</Col>
+          <SplitPane split='vertical' resizerStyle={styles}>
+            <div initialSize='25%' minSize='20%'>
+              {props.leftLayout}
+            </div>
+            <div>
+              <div>{props.toolBar}</div>
+              <div>{props.contextLayout}</div>
+            </div>
+          </SplitPane>
         </Row>
       </Grid>
+      {/* <SplitPane split='vertical' minSize={50} defaultSize={100}>
+        <Pane initialSize='25%' minSize='10%' maxSize='500px'>
+          {props.leftLayout}
+        </Pane>
+        <Pane>{props.contextLayout}</Pane>
+      </SplitPane>
+  */}
     </div>
   )
 }
